a9bc28d67ff079fcb8b683caa615e3781c7d5baf
Rick Companje <rick@companje.nl>
2020-05-25 11:23:44 +0200

    Remove certain fields from output and format relations

--- index.js
+++ index.js
@@ -27,11 +27,56 @@ stream.pipe(replaceStream('<ZR>', '\n')).pipe(parser); //fix incorrect XML befor
 
 parser.on('opentag', function(name, attrs) {
   tag = name.toLowerCase();
-  if (name=='AHD') {
+  if (name=='AHD') { //mis je zo het laatste item niet?
     if (item && item["skipoutput"]!="Ja") {
 
-      // item.GUID = item.guid;
-      // delete item.guid;
+      delete item["skipoutput"];
+      delete item["canmakephysic"];
+      delete item["fnc_bep_toegang"];
+      delete item["basismap"];
+      delete item["opslaglokatie"];
+      delete item["ahd_id_top"];
+      delete item["reageer_email"];
+      delete item["voorlopig_nummer"];
+      delete item["volgnummer"];
+      delete item["adt_id"];
+      delete item["kot_code"];
+      delete item["uitleenprioriteit"];
+      delete item["origineel"];
+      delete item["datecreated"];
+      delete item["datemutated"];
+      delete item["usercreated"];
+      delete item["usermutated"];
+      delete item["oid"];
+      delete item["auditheaders"];
+      delete item["aantuitvorm"];
+      delete item["betaaldeprijs_1"];
+      delete item["naamvorigeeigenaar_1"];
+      delete item["vermeldinjaarverslag_1"];
+      delete item["verworvenvanop_1"];
+      delete item["wijzevanverwerving_1"];
+      delete item["overige_bestanden"];
+      delete item["bestand_padnaam"];
+      delete item["bestand_pad_is_basispad"];
+      delete item["bestand_bestandsnaam"];
+      delete item["beschrijver_naam_1"];
+      delete item["beschrijver_datum_1"];
+
+      // for (const i in item["relaties"]) {
+      //   const rel = item["relaties"][i];
+      //   if (/PAP/.test(rel.rel_stuk_aet_code)) {
+      //     //item["relaties"].splice(i,1); //
+      //     delete item["relaties"][i]; 
+      //   } else {
+      //     const aet_code = (rel.rel_stuk_aet_code || rel.rel_top_aet_code).toLowerCase();
+      //     item["relaties"][i] = rel.rel_adt_id + "/" + aet_code + "/" + rel.ahd_id_rel;
+      //   }
+      // }
+      if (Array.isArray(item["relaties"])) {
+        item["relaties"] = item["relaties"].map(item => /PAP/.test(item.rel_stuk_aet_code) ? null : item.rel_adt_id + "/" + item.rel_top_code + "/" + item.ahd_id_rel);
+        item["relaties"] = item["relaties"].filter(item => item!=null);
+      }
+
 
       console.log(JSON.stringify(item,null,4) + ",");
     }
@@ -41,7 +86,7 @@ parser.on('opentag', function(name, attrs) {
     item = { GUID:"" };
     // items.push(item);
   }
-  else if (/<AWE>|<ABD>|<REL>|<TWD>|<FVD>|<AGR>|<SOS>|<SBK>/.test("<"+name+">")) {
+  else if (/<AWE>|<ABD>|<REL>|<TWD>|<TWE>|<FVD>|<AGR>|<SOS>|<SBK>/.test("<"+name+">")) {
     //er volgt nu een sub_item in de XML. sub_items staan in de XML meestal buiten/na de AHD behalve bij AWE.
     //toch horen ze bij de AHD waar ze direct na komen.
     is_sub = true;
@@ -63,14 +108,15 @@ parser.on('closetag', function(name) {
 parser.on('text', function(text) {
   if (!item) return;
   else if (!is_sub) {
-    if (tag=="guid") tag="GUID";
+    if (tag=="guid") tag=tag.toUpperCase();
+    if (tag=="aet") text=text.toLowerCase();
     item[tag] = text; //is property binnen een AHD
   }
   else if (sub_tag=="AWE") {
     // if (!item["flexvelden"]) item["flexvelden"] = {};
     if (tag=="naam") veldnaam = text.toLowerCase();
     // else if (tag=="waarde") item["flexvelden"][veldnaam] = text
-    else if (tag=="waarde") item[veldnaam] = text
+    else if (tag=="waarde") item[""+veldnaam] = text
   }
   else if (sub_tag=="ABD") {
     // if (!item["bestand"]) item["bestand"] = sub_item;
@@ -85,8 +131,6 @@ parser.on('text', function(text) {
     if (!item["relaties"]) item["relaties"] = [];
     sub_item[tag] = text;
   } 
-
-
 
   // else if (sub_tag=="SBK") {    
   //   if (!item["onderliggende_archiefeenheidssoorten"]) item["onderliggende_archiefeenheidssoorten"] = {}; //archiefeenheidssoorten
